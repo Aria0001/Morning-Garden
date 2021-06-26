04b299d5c2a65e61af06d2a6637876b4f04d743c
vickywsa12 <84848678+vickywsa12@users.noreply.github.com>
2021-06-26 13:29:32 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/Morning/Morning_Garden/';
+$config['base_url'] = 'http://localhost/Morning-Garden/';
 
 /*
 |--------------------------------------------------------------------------
